91f10df65cc90876c2935fdf444592fcf6feabeb
Dhruv <dhruvsavla03@gmail.com>
2024-08-21 11:59:43 +0530

    Update streamlitt.py

--- streamlitt.py
+++ streamlitt.py
@@ -1,14 +1,29 @@
 import streamlit as st
 import hashlib
 
+# Set the background image
+st.markdown("""
+<style>
+body {
+    background-image: url("https://images.unsplash.com/photo-1593642634443-44a065d409b6?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max");
+    background-size: cover;
+}
+</style>
+""", unsafe_allow_html=True)
+
 # Create a title for the app
 st.title("Solar SCADA Login")
 
 # Create a form for login credentials
 with st.form("login_form"):
-    username = st.text_input("Username")
-    password = st.text_input("Password", type="password")
-    submit_button = st.form_submit_button("Login")
+    st.write("Login to your Solar SCADA account")
+    username = st.text_input("Username", placeholder="Enter your username")
+    password = st.text_input("Password", type="password", placeholder="Enter your password")
+    st.write("")
+    st.write("")
+    st.markdown("Forgot password? [Click here](https://example.com/forgot-password)")
+    st.markdown("Don't have an account? [Register here](https://example.com/register)")
+    submit_button = st.form_submit_button("Login", button_color="green")
 
 # Create a dictionary to store usernames and passwords
 users = {"admin": "password123", "user1": "password456", "user2": "password789"}
@@ -35,12 +50,3 @@ if submit_button:
         # You can add code here to redirect to a new page or perform some action
     else:
         st.error("Invalid username or password")
-
-# Add a forgot password link
-st.markdown("Forgot password? [Click here](https://example.com/forgot-password)")
-
-# Add a register link
-st.markdown("Don't have an account? [Register here](https://example.com/register)")
-
-
-
